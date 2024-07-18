ed93ef96d747034dce778f79983ba7fe889e696a
Modasser Hasan <manvstech2.0@gmail.com>
2024-07-18 18:13:26 +0600

    Update

--- server-side/index.js
+++ server-side/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://chatter-box-x.vercel.app"],
+    origin: ["http://localhost:5173", "https://payeer-theta.vercel.app"],
     credentials: true,
   })
 );
